08811988a02e0229a8d0cdc4a6c712f9fdd6928d
Jahongir7 <Jahongir95119@gmail.com>
2021-07-08 09:22:40 +0500

    Zoom pic

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-import Start from './components/Start';
-import Question from './components/Question';
-import End from './components/End';
-import Modal from './components/Modal';
-import quizData from './data/quiz.json';
-
-let interval;
+import React, { useRef } from "react";
+import "./App.css";
 
 const App = () => {
-  const [step, setStep] = useState(1);
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [answers, setAnswers] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    if(step === 3) {
-      clearInterval(interval);
-    }
-  }, [step]);
-
-  const quizStartHandler = () => {
-    setStep(2);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
-
-  const resetClickHandler = () => {
-    setActiveQuestion(0);
-    setAnswers([]);
-    setStep(2);
-    setTime(0);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
+  const imgDiv = useRef();
+  const handleMouseMove = (e) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const x = ((e.pageX - left) / width) * 100;
+    const y = ((e.pageY - top) / height) * 100;
+    imgDiv.current.style.backgroundPosition = `${x}% ${y}%`;
+  };
 
   return (
     <div className="App">
-      {step === 1 && <Start onQuizStart={quizStartHandler} />}
-      {step === 2 && <Question 
-        data={quizData.data[activeQuestion]}
-        onAnswerUpdate={setAnswers}
-        numberOfQuestions={quizData.data.length}
-        activeQuestion={activeQuestion}
-        onSetActiveQuestion={setActiveQuestion}
-        onSetStep={setStep}
-      />}
-      {step === 3 && <End 
-        results={answers}
-        data={quizData.data}
-        onReset={resetClickHandler}
-        onAnswersCheck={() => setShowModal(true)}
-        time={time}
-      />}
-
-      {showModal && <Modal 
-        onClose={() => setShowModal(false)}
-        results={answers}
-        data={quizData.data}
-      />}
+      <div
+        className="img-container"
+        onMouseMove={handleMouseMove}
+        ref={imgDiv}
+        onMouseLeave={() =>
+          (imgDiv.current.style.backgroundPosition = `center`)
+        }
+      />
     </div>
   );
-}
+};
 
 export default App;
